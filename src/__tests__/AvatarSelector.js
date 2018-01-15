1ac208f3e4f0bfa43a50c38cd49bd34362fcd765
rayzoor12 <emilwow22@gmail.com>
2018-01-15 13:52:30 +0100

    Testing

---
+++ src/__tests__/AvatarSelector.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'enzyme';
+import AvatarSelector from '../components/AvatarSelector';
+
+test('renders the AvatarSelector', () => {
+  render(<AvatarSelector />);
+});
