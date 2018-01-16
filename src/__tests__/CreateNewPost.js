5211b8aa7b1c9505b74e57dbf1fb7956f698586a
Eddie Jostell <eddie.jostell@gmail.com>
2018-01-16 10:27:20 +0100

    Small update to test/CreateNewPost

--- src/__tests__/CreateNewPost.js
+++ src/__tests__/CreateNewPost.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import CreateNewPost from '../components/CreateNewPost';
+import App from '../components/App';
 
 describe('<CreateNewPost', () => {
     it('Load Create Form', () => {
+        const wrapper = mount(<App />);
         render(<CreateNewPost />);
     })
 })
