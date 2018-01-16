413ec9a6a0e73089f0eaf8d47b8b456b53ecdc4c
Eddie Jostell <eddjos@saltsea.net>
2018-01-16 17:59:21 +0100

    Updated CreateNewPost

--- src/__tests__/CreateNewPost.js
+++ src/__tests__/CreateNewPost.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import CreateNewPost from '../components/CreateNewPost';
-import App from '../components/App';
+import Posts from '../components/Posts';
 
 describe('<CreateNewPost', () => {
-    it('Load Create Form', () => {
-        const wrapper = mount(<App />);
-        render(<CreateNewPost />);
-    })
-})
+
+    it('Form was loaded', () => {
+        const wrapper = shallow(<CreateNewPost author="" updatePosts="" />);
+        expect(wrapper.exists()).toBeTruthy();
+    });
+
+    it('A Post was created', () => {
+        const newMockPost = jest.fn();
+        const wrapper = mount(<Posts CreateNowPost={newMockPost} />);
+        const titleEvent = { target: { name: "title", value: "My Awesome Post" } };
+        const contentEvent = { target: { name: "content", value: "Awesome Content Right here! You Definetly need to read this!" } };
+        wrapper
+          .find('input[name="title"]')
+          .simulate("change", titleEvent);
+        wrapper
+          .find('textarea[name="content"]')
+          .simulate("change", contentEvent);
+        wrapper.find('form').simulate("submit");
+        // expect(wrapper.find(".success").exists()).toBeTruthy();
+        console.log(titleEvent);
+    });
+});
+
