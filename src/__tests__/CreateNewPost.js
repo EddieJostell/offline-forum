7afa46b05526606c37d09264309f593182b8fbb7
Eddie <eddie.jostell@gmail.com>
2018-01-17 17:36:54 +0100

    CreateNewPost done?!?!

--- src/__tests__/CreateNewPost.js
+++ src/__tests__/CreateNewPost.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import { shallow, mount, render } from 'enzyme';
-import CreateNewPost from '../components/CreateNewPost';
-import Posts from '../components/Posts';
+import React from "react";
+import { shallow, mount, render } from "enzyme";
+import CreateNewPost from "../components/CreateNewPost";
+import Posts from "../components/Posts";
 
-describe('<CreateNewPost', () => {
+describe("<CreateNewPost", () => {
+  it("Form was loaded", () => {
+    const author = "Morgana";
+    const wrapper = shallow(<CreateNewPost author={author} updatePosts={() => {}} />);
+    expect(wrapper.exists()).toBeTruthy();
+  });
 
-    it('Form was loaded', () => {
-        const wrapper = shallow(<CreateNewPost author="" updatePosts="" />);
-        expect(wrapper.exists()).toBeTruthy();
-    });
-
-    it('A Post was created', () => {
-        const newMockPost = jest.fn();
-        const wrapper = mount(<Posts CreateNowPost={newMockPost} />);
-        const titleEvent = { target: { name: "title", value: "My Awesome Post" } };
-        const contentEvent = { target: { name: "content", value: "Awesome Content Right here! You Definetly need to read this!" } };
-        wrapper
-          .find('input[name="title"]')
-          .simulate("change", titleEvent);
-        wrapper
-          .find('textarea[name="content"]')
-          .simulate("change", contentEvent);
-        wrapper.find('form').simulate("submit");
-        // expect(wrapper.find(".success").exists()).toBeTruthy();
-        console.log(titleEvent);
-    });
+  it("Post Created", () => {
+    const currentPersona = "Morgana";
+    const newMockPost = jest.fn();
+    const wrapper = mount(<Posts CreateNowPost={newMockPost} currentPersona={currentPersona} />);
+    const titleEvent = { target: { name: "title", value: "My Awesome Post" } };
+    const contentEvent = {
+      target: {
+        name: "content",
+        value: "Awesome Content Right here! You Definetly need to read this!"
+      }
+    };
+    wrapper.find('input[name="title"]').simulate("change", titleEvent);
+    wrapper.find('textarea[name="content"]').simulate("change", contentEvent);
+    wrapper.find("form").simulate("submit");
+    /* console.log(titleEvent.target.value);
+    console.log(contentEvent.target.value); */
+    console.log(wrapper.props());
+    expect(titleEvent.target.value).toBeDefined();
+    expect(contentEvent.target.value).toBeDefined();
+  });
 });
-
