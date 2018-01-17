f719772d474007de4528df35e0aebfc2591f151a
rayzoor12 <emil.f@acne.se>
2018-01-17 17:14:35 +0100

    app.js

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, mount } from 'enzyme';
 import App from '../components/App';
+import PersonaSwitcher from '../components/PersonaSwitcher';
 
 test('renders the app', () => {
   render(<App />);
 });
+
+
+describe("<PersonaSwitcher />", () => {
+  it("Switching persona", () => {
+    const wrapper = mount(<App />);
+    wrapper.setState({ currentPersona: "Morgana" });
+    const personaState = wrapper.state().currentPersona;
+    console.log(personaState);
+    expect(personaState).toBe("Morgana")
+    const wrapperPersona = mount(<PersonaSwitcher currentPersona={personaState} changePersona={() => {}} />)
+    console.log(wrapperPersona.props().currentPersona);
+    const selectWrapper = wrapperPersona.find('[data-selectnpc="select"]');
+    selectWrapper.simulate('change', {target : { value : "Morgana"}});
+    const newState = wrapper.state().currentPersona;
+    console.log(newState);
+    expect(wrapperPersona.props().currentPersona).toBe("Morgana");
+  }); 
+});
+/* wrapperPersona.setProps({ currentPersona: 'Morgana' }); */
--- src/__tests__/PersonaSwitcher.js
+++ src/__tests__/PersonaSwitcher.js
@@ -4,10 +4,12 @@ import PersonaSwitcher from '../components/PersonaSwitcher';
 
 
 describe("<PersonaSwitcher />", () => {
-  it("renders an image", () => {
-    const wrapper = mount(<PersonaSwitcher currentPersona="Morgana" changePersona={() => {}} />);
-    const selectWrapper = wrapper.find('[data-selectnpc="select"]');   
-    selectWrapper.simulate('change', {target : { value : "Zac"}});
-    expect(wrapper.props().currentPersona).toBe("Zac");
+  it("Switching persona", () => {
+    const wrapper = mount(<PersonaSwitcher currentPersona="Zac" changePersona={() => {}} />);
+    const selectWrapper = wrapper.find('[data-selectnpc="select"]');
+    
+    selectWrapper.simulate('change', {target : { value : "Morgana"}});
+    wrapper.setProps({ currentPersona: 'Morgana' });
+    expect(wrapper.props().currentPersona).toBe("Morgana");
   }); 
 });
