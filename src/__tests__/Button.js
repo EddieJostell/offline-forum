1251946b67763b1002eb5216dd6bee489e2e1ae7
Eddie <eddie.jostell@gmail.com>
2018-01-17 23:31:30 +0100

    Stupid button...

--- src/__tests__/Button.js
+++ src/__tests__/Button.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import { shallow, mount, render } from 'enzyme';
-import Button from '../components/Button';
+import React from "react";
+import { shallow, mount, render } from "enzyme";
+import Button from "../components/Button";
 
-describe('<Button />', () => {
-    it.skip('render the Button', () => {
-        render(<Button />);
-    });
+describe("<Button />", () => {
+  it.skip("render the Button", () => {
+    render(<Button />);
+  });
 });
 
-it.skip('check if HOC Button exists', () => {
-    const btn = shallow(<Button />);
-    expect(btn.find('[data-test="button"]').exists());
-    /*
-expect(wrapper.find('[data-test="button"]')
-    .hasClass('cursor-not-allowed'))
-    .toEqual(false);
-    */
-})
-
-it.skip('Snapshot of component', () => {
-    const btn = shallow(<Button onClick="onClick" />);
-    expect(btn).toMatchSnapshot();
+it.skip("check if HOC Button exists", () => {
+  const btn = shallow(<Button />);
+  expect(btn.find('[data-test="button"]').exists());
 });
 
-it('Button has props', () => {
-    const btn = shallow(<Button />);
-    console.log(btn.props());
-    expect(btn.props()).toBeTruthy();
-})
+it.skip("Snapshot of component", () => {
+  const btn = render(<Button onClick="onClick" />);
+  expect(btn).toMatchSnapshot();
+});
 
+it("Button has props", () => {
+  const btn = shallow(<Button />);
+  //  console.log(btn.props());
+  expect(btn.props()).toBeTruthy();
+});
 
+it("Button has CSS", () => {
+  const btn = shallow(<Button />);
+  expect(btn.props().className).toEqual(
+    "bg-indigo-dark hover:bg-indigo-darker text-white font-bold py-2 px-4 rounded-full "
+  );
+});
