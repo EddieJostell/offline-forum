9d0cf57b08a16011240a74223d104aa66f629cd6
Eddie <eddie.jostell@gmail.com>
2018-01-21 23:01:13 +0100

    SinglePost.js

---
+++ src/__tests__/SinglePost.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { shallow, mount, render } from "enzyme";
+import SinglePost from '../components/SinglePost';
+
+describe('<SinglePost />', () => {
+    it('Component should exist', () => {
+
+    })
+})
+
