5fc8195d644c0172068b24579911fea9a51efd63
Eddie Jostell <eddie.jostell@gmail.com>
2018-01-25 15:56:46 +0100

    Sorted Comments.js and CreateNewPost.js

--- src/__tests__/Comments.js
+++ src/__tests__/Comments.js
@@ -4,9 +4,6 @@ import Comments from "../components/Comments";
 
 describe("Comments", () => {
   it("Fetch a comment", () => {
-    const commentWrapper = mount(
-      <Comments postId="lks334" currentPersona="Morgana" />
-    );
     const mockItem = [
       {
         comment: "Morgana of Castle Camelot",
@@ -17,10 +14,36 @@ describe("Comments", () => {
       }
     ];
     const mockery = JSON.stringify(mockItem);
-    // console.log(mockery);
     localStorage.setItem("comments", mockery);
-    commentWrapper.instance().setCommentsFromLocalStorage("lks334");
-    expect(commentWrapper.state().comments[0].id).toEqual("2287qw");
+    const commentWrapper = mount(
+      <Comments postId="lks334" currentPersona="Morgana" />
+    );
+    expect(commentWrapper.state().comments[0].comment).toEqual(
+      "Morgana of Castle Camelot"
+    );
+  });
+
+  it("should render my content and date", () => {
+    const mockItem = [
+      {
+        comment: "Morgana of Castle Camelot",
+        id: "2287qw",
+        postId: "lks334",
+        author: "Morgana",
+        date: "1/23/2018, 03:10:32 AM"
+      }
+    ];
+    const mockery = JSON.stringify(mockItem);
+    localStorage.setItem("comments", mockery);
+    const commentWrapper = mount(
+      <Comments postId="lks334" currentPersona="Morgana" />
+    );
+    const contentString = commentWrapper
+      .find(".text-grey-darker")
+      .text();
+    const dateString = commentWrapper.find('.text-grey-dark').text();
+    expect(contentString).toEqual("Morgana of Castle Camelot");
+    expect(dateString).toContain("1/23/2018, 03:10:32 AM");
   });
 
   it("Render list of comments", () => {
@@ -49,17 +72,12 @@ describe("Comments", () => {
     commentWrapper
       .instance()
       .renderCommentList(commentWrapper.state().comments);
-    /* console.log(
-      commentWrapper
-        .instance()
-        .renderCommentList(commentWrapper.state().comments)
-    ); */
     expect(commentWrapper.state().comments).toHaveLength(2);
   });
 
   it("Remove a comment", () => {
     const commentWrapper = mount(
-      <Comments postId="lks334" currentPersona="Morgana" />
+      <Comments postId="lks334" currentPersona="Esmeralda" />
     );
     const mockItem = [
       {
@@ -71,12 +89,14 @@ describe("Comments", () => {
       }
     ];
     const mockery = JSON.stringify(mockItem);
-    // console.log(mockery);
     localStorage.setItem("comments", mockery);
     commentWrapper.instance().setCommentsFromLocalStorage();
-    console.log(commentWrapper.state().comments);
+    expect(commentWrapper.state().comments).toHaveLength(1);
     commentWrapper.instance().removeComment("117as");
-    console.log(commentWrapper.state().comments);
     expect(commentWrapper.state().comments).not.toHaveLength(1);
+  });
+
+  afterEach(() => {
+    localStorage.clear();
   });
 });
--- src/__tests__/CreateNewPost.js
+++ src/__tests__/CreateNewPost.js
@@ -5,87 +5,43 @@ import Posts from "../components/Posts";
 
 describe("<CreateNewPost", () => {
   it("Form was loaded", () => {
-    const author = "Morgana";
     const wrapper = shallow(
-      <CreateNewPost author={author} updatePosts={() => {}} />
+      <CreateNewPost author="Morgana" updatePosts={jest.fn()} />
     );
-    expect(wrapper.exists()).toBeTruthy();
+    expect(wrapper.find('form').exists()).toEqual(true);
   });
+
+    it("submit should fire", () => {
+      const onSubmit = jest.fn();
+      const wrapper = mount(<CreateNewPost author="Morgana" updatePosts={onSubmit} />);
+
+      wrapper.find('[data-type="form"]').simulate("submit");
+      expect(onSubmit).toHaveBeenCalledTimes(1);
+    });
 
   it("Create a Post and change state onchange title & content", () => {
     // const newMockUpdate = jest.fn();
     const wrapper = mount(
       <CreateNewPost
         author="Morgana"
-        updatePosts=""
+        updatePosts={jest.fn()}
       />
     );
-
-    const titleEvent = { target: { name: "title", value: "My Awesome Post" } };
-    const contentEvent = {
+    const title = { target: { name: "title", value: "My Awesome Post" } };
+    const content = {
       target: {
         name: "content",
         value: "Awesome Content Right here! You Definetly need to read this!"
       }
     };
-    wrapper.find('input[name="title"]').simulate("change", titleEvent);
-    wrapper.find('textarea[name="content"]').simulate("change", contentEvent);
+    wrapper.find('input[name="title"]').simulate("change", title);
+    wrapper.find('textarea[name="content"]').simulate("change", content);
    // wrapper.find('[data-type="form"]').simulate("submit");
-    
     expect(wrapper.state().title).toEqual("My Awesome Post");
     expect(wrapper.state().content).toEqual("Awesome Content Right here! You Definetly need to read this!");
   
   });
 
-  /* it("Can Create a Post with only a title", () => {
-    const currentPersona = "Zac";
-    const newMockPost = jest.fn();
-    const wrapper = mount(
-      <Posts CreateNewPost={newMockPost} currentPersona={currentPersona} />
-    );
-    const titleEvent = { target: { name: "title", value: "My Awesome Post" } };
-    const contentEvent = { target: { name: "content", value: null } };
-    wrapper.find('input[name="title"]').simulate("change", titleEvent);
-    wrapper.find('[data-type="form"]').simulate("submit");
-    wrapper.setState({
-      title: titleEvent.target.value,
-      content: contentEvent.target.value
-    });
 
-    expect(wrapper.state()).toBeDefined();
-    expect(wrapper.state().content).toBe(null);
-    expect(currentPersona).toBe("Zac");
-    expect(wrapper.find(Posts)).toHaveLength(1);
-  })
 
-  it("Can Create a Post with only content", () => {
-    const currentPersona = "Zac";
-    const newMockPost = jest.fn();
-    const wrapper = mount(
-      <Posts CreateNewPost={newMockPost} currentPersona={currentPersona} />
-    );
-    const titleEvent = { target: { name: "title", value: null } };
-    const contentEvent = {
-      target: {
-        name: "content",
-        value: "OMG!!, So like I met this guy like and he was really cute like."
-      }
-    };
-    // wrapper.find('input[name="title"]').simulate("change", titleEvent);
-    wrapper.find('textarea[name="content"]').simulate("change", contentEvent);
-    wrapper.find('[data-type="form"]').simulate("submit");
-    expect(titleEvent.target.value).toBe(null);
-    expect(contentEvent.target.value).toBeDefined();
-    expect(wrapper.find(Posts)).toHaveLength(1);
-  });
-
-  it("Can Create a Post if no inputs are activated", () => {
-    const currentPersona = "Zac";
-    const newMockPost = jest.fn();
-    const wrapper = mount(
-      <Posts CreateNewPost={newMockPost} currentPersona={currentPersona} />
-    );
-    wrapper.find('[data-type="form"]').simulate("submit");
-    expect(wrapper.find(Posts)).toHaveLength(1);
-  }); */
 });
