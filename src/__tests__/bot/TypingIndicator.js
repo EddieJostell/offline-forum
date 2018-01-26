98334f12fd2a02ccb95e1daa2dc8737f997b36d7
Eddie Jostell <eddie.jostell@gmail.com>
2018-01-26 14:24:10 +0100

    Bot.js done TypingIndicator.js done

--- src/__tests__/bot/TypingIndicator.js
+++ src/__tests__/bot/TypingIndicator.js
@@ -2,21 +2,17 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import TypingIndicator from '../../components/Bot/TypingIndicator';
 
-
-
-
 describe('<TypingIndicator', () => {
 
-  let wrapper = mount(<TypingIndicator />);
+    let wrapper = mount(<TypingIndicator />);
 
   beforeEach(() => {
-     wrapper = mount(<TypingIndicator />);
+    wrapper = mount(<TypingIndicator />);
   });
 
 
     it('should not be visible on mount', () => {
-      expect(wrapper.html()).toEqual(null);
-        
+      expect(wrapper.html()).toEqual(null);   
     })
 
     it('should be visible if state "typing" equal true', () => {
