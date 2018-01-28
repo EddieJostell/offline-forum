a1d2ce7305f7bb14e42f209e08c39750796351a7
Eddie <eddie.jostell@gmail.com>
2018-01-28 15:04:02 +0100

    e2e should make avatars remove their posts

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -1,3 +1,5 @@
+import { closeSync } from "fs";
+
 describe("App.js", () => {
   context("Creating Post(s)", () => {
     beforeEach(() => {
@@ -7,10 +9,10 @@ describe("App.js", () => {
     it("should count posts", () => {
       cy.get('[data-type="post"]').should("have.length", 3);
     });
-    it.skip("should make a post", () => {
+    it("should make a post", () => {
       cy.get('[data-type="post"]').should("have.length", 3);
-      cy.get('[name="title"]').type("Hello World", { delay: 50 });
-      cy.get('[name="content"]').type("Funka plx", { delay: 50 });
+      cy.get('[name="title"]').type("Hello World", { delay: 200 });
+      cy.get('[name="content"]').type("Funka plx", { delay: 200 });
       cy.get('[value="Create"]').click({});
       cy.get('[data-type="post"]').should("have.length", 4);
     });
@@ -31,7 +33,7 @@ describe("App.js", () => {
       cy.get(".block").select("Zac");
       cy
         .get(":nth-child(2) > div.py-2 > .container > #comment")
-        .type("Such good post much WOW!", { delay: 100 });
+        .type("Such good post much WOW!", { delay: 200 });
 
       cy
         .get(":nth-child(2) > :nth-child(2) > .container > .bg-indigo-dark")
@@ -48,31 +50,46 @@ describe("App.js", () => {
 
         it("should do as said in the context ", () => {
           cy.get('[data-type="comment"]').should("have.length", 3);
-          cy.get(".block").select("Zac");
+          cy.get(".block").select("Zac", { delay: 200 });
           cy
             .get(":nth-child(2) > div.py-2 > .container > #comment")
-            .type("I am Zac and I approve of this post!!", { delay: 100 });
+            .type("I am Zac and I approve of this post!!", { delay: 200 });
           cy
             .get(
               ":nth-child(2) > :nth-child(2) > .container > .bg-indigo-dark"
             )
             .click();
-            cy.get('.block').select("Esmeralda");
+            cy.get(".block").select("Esmeralda", { delay: 200 });
             cy
               .get(
                 ":nth-child(3) > div.py-2 > .container > #comment"
               )
               .type("I am Esmeralda and I approve of this post!!", {
-                delay: 100
+                delay: 200
               });
               cy
                 .get(
                   ":nth-child(3) > :nth-child(2) > .container > .bg-indigo-dark"
                 )
                 .click();
-              
-
         });
+
+         
+                it("should make avatars remove their posts", () => {
+                   cy
+                     .get(".block")
+                     .select("Esmeralda", { delay: 200 });
+                  cy.get(".bg-red-dark").click();
+                  cy
+                    .get(".block")
+                    .select("Zac", { delay: 200 });
+                  cy.get(".bg-red-dark").click();
+                  cy
+                    .get(".block")
+                    .select("Morgana", { delay: 200 });
+                     cy.get(".bg-red-dark").click();
+                });
+
       }
     );
   });
