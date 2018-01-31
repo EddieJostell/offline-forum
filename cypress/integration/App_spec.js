eaf49cff498f98520dc4a4550883850b8de7bb79
rayzoor12 <emil.f@acne.se>
2018-01-31 14:34:45 +0100

    till ediie från emil

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -14,6 +14,7 @@ describe("App.js", () => {
       cy.get('[name="title"]').type("Hello World", { delay: 100 });
       cy.get('[name="content"]').type("Funka plx", { delay: 100 });
       cy.get('[value="Create"]').click({});
+      cy.wait(1000)
       cy.get('[data-type="post"]').should("have.length", 4);
     });
   });
@@ -33,6 +34,8 @@ describe("App.js", () => {
       cy
         .get(":nth-child(2) > :nth-child(2) > .container > .bg-indigo-dark")
         .click();
+      cy.get(':nth-child(2) > :nth-child(2) > .text-indigo-darker').scrollIntoView()
+      cy.wait(1000)
       cy.get('[data-type="comment"]').should("have.length", 4);
     });
 
@@ -41,18 +44,18 @@ describe("App.js", () => {
       () => {
         it("should do as said in the context ", () => {
           cy.get('[data-type="comment"]').should("have.length", 3);
-          cy.get(".block").select("Zac", { delay: 100 });
+          cy.get(".block").select("Zac", { delay: 50 });
           cy
             .get(":nth-child(2) > div.py-2 > .container > #comment")
-            .type("I am Zac and I approve of this post!!", { delay: 100 });
+            .type("I am Zac and I approve of this post!!", { delay: 50 });
           cy
             .get(":nth-child(2) > :nth-child(2) > .container > .bg-indigo-dark")
             .click();
-          cy.get(".block").select("Esmeralda", { delay: 100 });
+          cy.get(".block").select("Esmeralda", { delay: 50 });
           cy
             .get(":nth-child(3) > div.py-2 > .container > #comment")
             .type("I am Esmeralda and I approve of this post!!", {
-              delay: 100
+              delay: 50
             });
           cy
             .get(":nth-child(3) > :nth-child(2) > .container > .bg-indigo-dark")
@@ -60,11 +63,11 @@ describe("App.js", () => {
         });
 
         it("should make avatars remove their posts", () => {
-          cy.get(".block").select("Esmeralda", { delay: 100 });
+          cy.get(".block").select("Esmeralda", { delay: 50 });
           cy.get(".bg-red-dark").click();
-          cy.get(".block").select("Zac", { delay: 100 });
+          cy.get(".block").select("Zac", { delay: 50 });
           cy.get(".bg-red-dark").click();
-          cy.get(".block").select("Morgana", { delay: 100 });
+          cy.get(".block").select("Morgana", { delay: 50 });
           cy.get(".bg-red-dark").click();
           cy.wait(1000);
         });
