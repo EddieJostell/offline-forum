b1c04fd9f0bfdc50026462c4468f1c9822c0e91a
Eddie Jostell <eddie.jostell@gmail.com>
2018-02-01 13:46:42 +0100

    test on api/index.js function(s)

--- src/__tests__/api/index.js
+++ src/__tests__/api/index.js
@@ -5,24 +5,5 @@ describe('api', () => {
     const expected = [];
     expect(api.fetchPersonas()).toEqual(expect.arrayContaining(expected))
   });
-  it('createPostObject()', () => {
-    const title = "Hello";
-    const content = "Wold";
-    const author = "Zac";
-    const obj = {
-      title,
-      content,
-      id: api.generateID(),
-      author,
-      date: (new Date()).toLocaleString()
-    }
-    expect(api.createPostObject(title, content, author)).not.toMatchObject(obj);
-
-    
-  });
-  it('fetchPersonas()', () => {
-    const expected = [];
-    expect(api.fetchPersonas()).toEqual(expect.arrayContaining(expected))
-  });
 })
 
